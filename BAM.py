da86dbbde2b626c2c893258fa9fb2af0a1b94145
Qianyao Zheng <qianyaozheng@outlook.com>
2026-02-10 22:37:54 +0100

    binomial_option_trees.txt

--- BAM.py
+++ BAM.py
@@ -11,7 +11,6 @@ def binomial_option_trees(S0, u, d, r, t, strikes, option_type="call"):
 
     # create stock price tree
     S = np.zeros((t + 1, t + 1))
-    option_tree = np.zeros((t + 1, t + 1))
      # stock tree: S[i,n] where i=#up moves, n=time
     for n in range(t + 1):
         for i in range(n + 1):
@@ -36,13 +35,26 @@ def binomial_option_trees(S0, u, d, r, t, strikes, option_type="call"):
         # backward induction
         for n in range(t - 1, -1, -1):
             for i in range(n + 1):
-                H[i, n] = (1 / (1 + r)) * (q * H[i + 1, n + 1] + (1 - q) * V[i, n + 1])
+                H[i, n] = (1 / (1 + r)) * (q * H[i + 1, n + 1] + (1 - q) * H[i, n + 1])
 
         opt[K] = H
 
     return q, S, opt
 
-
+# Plotting function
+def plot_evolution_root(opt_trees, strikes, t, title, ylabel):
+    times = np.arange(t + 1)
+    plt.figure()
+    for K in strikes:
+        series = [opt_trees[K][0, n] for n in times]
+        plt.plot(times, series, marker="o", label=f"K={K}")
+    plt.xlabel("time step n")
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.xticks(times)
+    plt.legend()
+    plt.grid(True)
+    plt.show()
 
 
 # Input
@@ -56,7 +68,14 @@ print("risk-neutral q =", q)
 print("Stock tree S (rows i=#up, cols n=time):\n", S)
 
 
+# Plots (as required)
+plot_evolution_root(call_trees, strikes, t,
+                    title="Evolution of Call Option Price over Time (root node)",
+                    ylabel="Call price H[0,n]")
 
+plot_evolution_root(put_trees, strikes, t,
+                    title="Evolution of Put Option Price over Time (root node)",
+                    ylabel="Put price H[0,n]")
 
 
 
